1ffd56dbcff2c52177a9425880c1d66ff9bef729
Ian Doarn <ian.doarn@zimmerbiomet.com>
2017-10-03 17:41:25 -0500

    fixed othello.py being basically all commented out

--- othello.py
+++ othello.py
@@ -7,26 +7,24 @@ if __name__ == '__main__':
     game = GameBoard(dimensions=6)
     game.setup()
 
-    print(game)
+    init(autoreset=True)
 
-    # init(autoreset=True)
-    #
-    # print(Fore.LIGHTWHITE_EX + str(game))
-    #
-    # current_piece = BLACK_PIECE
-    #
-    # while game.is_setup:
-    #     print(Fore.LIGHTGREEN_EX + "Black : {}\tWhite : {}".format(
-    #         str(game.count_black_tiles),
-    #         str(game.count_white_tiles)
-    #     ))
-    #     coordinates = input("Enter coordinates for {} piece: ".format(current_piece))
-    #     coord = coordinates.split(' ')
-    #     x, y = int(coord[0]), int(coord[1])
-    #     if game.add_tile(x, y, current_piece):
-    #         system('cls')
-    #         print(Fore.LIGHTWHITE_EX + str(game))
-    #         game.game_state()
-    #         current_piece = game.opposite(current_piece)
-    #     else:
-    #         print("Invalid move: x:{} y:{}".format(str(x), str(y)))
+    print(Fore.LIGHTWHITE_EX + str(game))
+
+    current_piece = BLACK_PIECE
+
+    while game.is_setup:
+        print(Fore.LIGHTGREEN_EX + "Black : {}\tWhite : {}".format(
+            str(game.count_black_tiles),
+            str(game.count_white_tiles)
+        ))
+        coordinates = input("Enter coordinates for {} piece: ".format(current_piece))
+        coord = coordinates.split(' ')
+        x, y = int(coord[0]), int(coord[1])
+        if game.add_tile(x, y, current_piece):
+            system('cls')
+            print(Fore.LIGHTWHITE_EX + str(game))
+            game.game_state()
+            current_piece = game.opposite(current_piece)
+        else:
+            print("Invalid move: x:{} y:{}".format(str(x), str(y)))
